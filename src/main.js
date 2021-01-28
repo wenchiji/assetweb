5cd354d71d32d0fa0f26cf54ebebf37336b3d66c
wenchiji <1565699710@qq.com>
2021-01-28 17:16:54 +0800

    0218-超时退出-完成

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import store from './store'
 import './plugins/element.js'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import $ from 'jquery';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(axios);
 Vue.prototype.$http = axios;
+
+window.jQuery = $;
+window.$ = $;
 
 new Vue({
   router,
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -34,12 +34,43 @@
     export default {
         name: "index",
         methods:{
+
             logout(){
                 localStorage.removeItem('eleToken');
                 localStorage.removeItem('user');
+                // window.location.href="/logout";
                 this.$router.push("/")
             }
-        }
+        },
+        mounted() {
+            var that = this
+            //设置超时退出
+            var lastTime = new Date().getTime();
+            var currentTime = new Date().getTime();
+            var timeOut = 30 * 60 * 1000; //设置超时时间： 10分
+
+            $(function(){
+                /* 鼠标移动事件 */
+                $(document).mouseover(function(){
+                    lastTime = new Date().getTime(); //更新操作时间
+
+                });
+            });
+
+            function testTime(){
+                currentTime = new Date().getTime(); //更新当前时间
+                if(currentTime - lastTime > timeOut){ //判断是否超时
+                    console.log("超时");
+                    localStorage.removeItem("eleToken")
+                    that.$router.push({
+                        path:"/"
+                    })
+                }
+            }
+
+            /* 定时器  间隔1秒检测是否长时间未操作页面  */
+            window.setInterval(testTime, 1000);
+        },
     }
 </script>
 
